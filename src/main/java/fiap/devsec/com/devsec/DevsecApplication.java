9d829cbac3b08369714bad151b2d95079d9d62a7
Thiago S Adriano <tadriano.net@gmail.com>
2023-11-18 07:34:51 -0300

    first commit

---
+++ src/main/java/fiap/devsec/com/devsec/DevsecApplication.java
@@ -0,0 +1,13 @@
+package fiap.devsec.com.devsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/devsec/com/devsec/DevsecApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.devsec.com.devsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
